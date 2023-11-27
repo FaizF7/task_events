ce5a3dc46b82ec727f908bc3ec8c586c7b74184e
Faiz Fazaluddin <faiz.mf07@gmail.com>
2023-11-27 18:20:44 +0000

    added checkbox

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,36 +1,49 @@
-const enterButton = document.querySelector("#enter")
+// const enterButton = document.querySelector("#enter")
+const form = document.querySelector("#todo-form")
 const textInput = document.querySelector("#new-todo")
 const list = document.querySelector("#list")
 const dateButton = document.querySelector('#date')
 const dateText = document.querySelector("#date-content")
 
-let text = ""
-
-textInput.addEventListener( "input", (event)=>{
-    text = event.target.value
-})
-
-enterButton.addEventListener("click", (event)=>{
+form.addEventListener("submit", (event)=>{
     event.preventDefault();
-    let newListItem = document.createElement("li")
-    newListItem.innerText = text
-    let button = document.createElement("button")
-    button.textContent = "delete"
-    button.addEventListener("click", (event)=>
-{
-    console.log("delete")
-    button.parentNode.parentNode.removeChild(button.parentNode)
+    if (event.target[0].value){
+        let newListItem = document.createElement("li")
+
+        let listLabel = document.createElement("label")
+        listLabel.innerText = event.target[0].value
+        form.reset()
+
+        let checkbox = document.createElement("input")
+        checkbox.type="checkbox"
+        newListItem.appendChild(checkbox)
+        newListItem.appendChild(listLabel)
+
+
+        
+        let button = document.createElement("button")
+        button.textContent = "delete"
+        button.addEventListener("click", (event)=>{
+            button.parentNode.parentNode.removeChild(button.parentNode)
+        })
+
+        newListItem.appendChild(button)
+        list.appendChild(newListItem)
+    }
 })
 
-
-    newListItem.appendChild(button)
-    list.appendChild(newListItem)
-   
-})
-
-dateButton.addEventListener("click", () =>
-{
-    dateText.innerText = Date()
+let dateOff = true
+dateButton.addEventListener("click", () =>{
+    if (dateOff){
+        dateText.innerText = Date()
+        dateButton.textContent = "Hide Date"
+        dateOff = !dateOff
+    }
+    else{
+    dateText.innerText = ""
+    dateButton.textContent = "Show Date"
+    dateOff = !dateOff
+    }
 })
 
 
